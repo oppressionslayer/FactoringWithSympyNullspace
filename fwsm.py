28f84ce17900a6f021f9e8929a9e51021b56cf97
oppressionslayer <37212196+oppressionslayer@users.noreply.github.com>
2024-02-08 00:25:07 -0700

    Update fwsm.py
    Added LU Decomposition but it's not working, you just have to comment out the current gauss_elim and uncomment the other two lines if you want to try to fix it. It would really speed it up.

--- fwsm.py
+++ fwsm.py
@@ -59,6 +59,36 @@ def is_quadratic_residue(a, p):
     return legendre_symbol == 1
     
     
+def matrixMul(A, B):
+    TB = zip(*B)
+    return [[sum(ea*eb for ea,eb in zip(a,b)) for b in TB] for a in A]
+ 
+def pivotize(m):
+    """Creates the pivoting matrix for m."""
+    n = len(m)
+    ID = [[float(i == j) for i in range(n)] for j in range(n)]
+    for j in range(n):
+        row = max(range(j, n), key=lambda i: abs(m[i][j]))
+        if j != row:
+            ID[j], ID[row] = ID[row], ID[j]
+    return ID
+ 
+def LU(A):
+    """Decomposes a nxn matrix A by PA=LU and returns L, U and P."""
+    n = len(A)
+    L = [[0.0] * n for i in range(n)]
+    U = [[0.0] * n for i in range(n)]
+    P = pivotize(A)
+    A2 = matrixMul(P, A)
+    for j in range(n):
+        L[j][j] = 1.0
+        for i in range(j+1):
+            s1 = sum(U[k][j] * L[i][k] for k in range(i))
+            U[i][j] = A2[i][j] - s1
+        for i in range(j, n):
+            s2 = sum(U[k][j] * L[i][k] for k in range(j))
+            L[i][j] = (A2[i][j] - s2) / U[j][j]
+    return (L, U, P)
 
 def gauss_elim(M):
 #reduced form of gaussian elimination, finds rref and reads off the nullspace
@@ -245,7 +275,10 @@ def factorise(N, B=10000, I=10000000):
 
      factor_base, smooth_base, root_base = fb_sm(N,B,I)
      t_matrix = build_matrix(factor_base, smooth_base)
+     #L, U, P = LU(t_matrix)
+     #gauss_elim(U)  # Replace gauss_elim with LU decomposition
      gauss_elim(t_matrix)
+
      factor = sympy_nullspace_solve(t_matrix, factor_base, smooth_base, root_base, N)
      
      if is_prime(factor):
@@ -258,4 +291,5 @@ def factorise(N, B=10000, I=10000000):
      print(N, factor)
    
    return sorted(factors_list)   
+   
    
